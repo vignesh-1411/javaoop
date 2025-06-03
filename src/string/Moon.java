3eaabbf13b606b24785a80b58d6b9d9a84af4767
vignesh-1411 <vigneshponnurangam24@gmail.com>
2025-06-03 13:42:56 +0530

    initial commit

--- src/string/Moon.java
+++ src/string/Moon.java
@@ -9,16 +9,22 @@ public class Moon {
 		this.distance = distance;
 		this.size=size;
 	}
-	public boolean equals(Object o){//Object o=m1....upcasting 
+	public boolean equals(Object o){
+//		Object o=m1....upcasting 
+		
 		Moon m=(Moon)o;
-	//we re downcasting again to access the child class members...
+		
+//	we re downcasting again to access the child class members...
+		
 		return this.distance==m.distance && this.size==m.size;
 	}
 	
 	public static void main(String[] args) {
 		Moon m1=new Moon(90,9);
-		Moon m2=new Moon(80,8);
-		System.out.println(m1==m2);
+		Moon m2=new Moon(90,9);
+//		System.out.println(m1==m2);
+		
+		System.out.println(m1.equals(m2));
 	}
 
 }
